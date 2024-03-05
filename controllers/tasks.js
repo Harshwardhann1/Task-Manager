38ed7d6858ce0830b0841be05d56d22443e21305
Harsh Wardhan <wardhan071@gmail.com>
2024-03-05 13:18:24 +0530

    first commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,24 @@ const getAllTasks = (req, res) => {
   res.send('all items from the file ');
 };
 
+const createTask = (req, res) => {
+  res.send('create tasks');
+};
+
+const getTask = (req, res) => {
+  res.send('get single tasks');
+};
+const updateTask = (req, res) => {
+  res.send('update tasks');
+};
+const deleteTask = (req, res) => {
+  res.send('delete tasks');
+};
+
 module.exports = {
   getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
 };
